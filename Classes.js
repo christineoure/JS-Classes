11ebbfa102eab15b36de766b24a8c84ae49eea98
christineoure <ourechristine@gmail.com>
2022-07-15 12:13:49 +0300

    new changes

--- Classes.js
+++ Classes.js
@@ -1,12 +1,30 @@
+let kiosk = {
+    
+        fruit:"orange",
+        quantity:3,
+        fruitPriceList:{"orange":30, "mango":15, "avocado":40},
+        getTotalCost:function(avtivity){
+            return`${this.quantity} ${this.fruit}s for KES ${this.quantity*this.fruitPriceList[this.fruit]}.00`
+        }
+    
+}
+// var kioskCalc = new KioskCalc("orange",30)
+console.log(kiosk.getTotalCost("orange", 5))
+
 class KioskCalc{
     constructor(fruit, quantity){
         this.fruit = fruit
         this.quantity = quantity
-        this.fruitPriceList = {"orange":30, "mango":15, "Avocado":40};
+        this.fruitPriceList = {"orange":30, "mango":15, "avocado":40}
         this.getTotalCost = function(){
-            return`${this.quantity} ${this.fruit} for KES ${this.quantity*this.fruitPriceList[this.fruit]}.00`
-        }
+
+            if (this.fruitPriceList[this.fruit]===undefined){
+                return `${this.fruit} is not available`
+            }
+            return`${this.quantity} ${this.fruit}s for KES ${quantity*this.fruitPriceList[fruit]*this.quantity}`
+        
     }
 }
-var kioskCalc = new KioskCalc("orange",2)
-console.log(kioskCalc.getTotalCost())
+}
+var mykioskCalc = new KioskCalc("orange",10)
+console.log(mykioskCalc.getTotalCost());
